ca799f488ec08a5fdb64b3a1f70879c6c335d224
Srihariaditha <adithasrihari@gmail.com>
2022-11-14 09:13:55 +0530

    Initial Commit

---
+++ DA_ALGO/day1.rb
@@ -0,0 +1,8 @@
+# Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+# You must do it in place.
+# Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+# Output: [[1,0,1],[0,0,0],[1,0,1]]
+def set_zeroes(matrix)
+  row_count, col_count = 0, 0
+  puts row_count
+end
